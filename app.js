7784336b9e179c03522fa8e1a06ff2f8a7648644
Noah Rijkaard <noahrijkaard@gmail.com>
2022-08-21 19:01:54 +0800

    Add script to create grid
    Add functionality for user to determine size of grid.
    Change colour of grid when hovered over squares.

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
 const row = document.createElement('div');
-const column = document.createElement('div');
+const box = document.createElement('div');
 const container = document.querySelector('.container');
-
 row.classList.add('row');
-column.classList.add('column');
+box.classList.add('box');
 
-row.textContent = 'row';
-column.textContent = 'column';
+function addBoxListeners(){
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', ()=> {
+            box.classList.add('changeColour');
+        }) 
+    });
+}
+
+function setCanvas(){
+    const size = prompt("Enter the size of the canvas");
+    if(size > 100){
+        size = 100;
+    }
+    // Remove all current boxes from container
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    // Create new boxes
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            // box.style.width = `${100/size}%`;
+            // box.style.height = `${100/size}%`;
+            row.appendChild(box.cloneNode(true));
+        }
+        container.appendChild(row.cloneNode(true));
+    }
+
+    addBoxListeners();
+}
+
+
 
 // Create a 16 by 16 grid of divs inside the container div
-
-for(let i = 0; i < 16; i++) {
-    row.appendChild(column.cloneNode(true));
+function createInitCanvas(){
+    for(let i = 0; i < 16; i++) {
+        row.appendChild(box.cloneNode(true));
+    }
+    for(let j = 0; j < 16; j++) {
+        container.appendChild(row.cloneNode(true));
+    }
 }
-for(let j = 0; j < 16; j++) {
-    container.appendChild(row.cloneNode(true));
-}
 
+
+addBoxListeners();
