7992e2f1b0c17fab8329390a0dee6dd47e88e7cf
Noah Rijkaard <noahrijkaard@gmail.com>
2022-08-25 21:35:01 +0800

    Add mutli colour + box sizing issue fix

--- app.js
+++ app.js
@@ -6,29 +6,41 @@ box.classList.add('box');
 
 function addBoxListeners(){
     const boxes = document.querySelectorAll('.box');
+    
+    
     boxes.forEach(box => {
         box.addEventListener('mouseover', ()=> {
-            box.classList.add('changeColour');
+            const color = randomColor();
+            let brightness = 100;
+            if(!box.classList.contains('passed')){
+                box.setAttribute('style', `background-color: ${color}`);
+                box.classList.add('passed');
+            }else{
+                brightness = brightness - 10;
+                box.setAttribute('style', `background-color: ${color}; filter: brightness(${reduceBrightness(brightness)}%);`);
+            }
         }) 
     });
 }
 
 function setCanvas(){
     const size = prompt("Enter the size of the canvas");
+    console.log("size", size);
     if(size > 100){
         size = 100;
     }
+
+    console.log("Size after check", size);
     // Remove all current boxes from container
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
     // Create new boxes
-    for(let i = 0; i < size; i++){
-        for(let j = 0; j < size; j++){
-            // box.style.width = `${100/size}%`;
-            // box.style.height = `${100/size}%`;
-            row.appendChild(box.cloneNode(true));
-        }
+
+    for(let i = 0; i < size; i++) {
+        row.appendChild(box.cloneNode(true));
+    }
+    for(let j = 0; j < size; j++) {
         container.appendChild(row.cloneNode(true));
     }
 
@@ -45,6 +57,14 @@ function createInitCanvas(){
     for(let j = 0; j < 16; j++) {
         container.appendChild(row.cloneNode(true));
     }
+}
+
+function randomColor(){
+    return '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+}
+
+function reduceBrightness(brightness){
+    return brightness >0 ? brightness - 10 : 0;
 }
 
 createInitCanvas();
